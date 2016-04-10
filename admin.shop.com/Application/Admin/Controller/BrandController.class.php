1799e4fc7f9bb650cb7a3af326defbab5124e605
tcpan <1037606804@qq.com>
2016-04-10 09:26:38 +0800

    品牌的管展示页面

---
+++ admin.shop.com/Application/Admin/Controller/BrandController.class.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Admin\Controller;
+
+/**
+ * 品牌模块
+ *
+ * @author PR
+ */
+class BrandController extends \Think\Controller {
+
+    private $_model = null;
+
+    protected function _initialize() {
+        $meta_title = array(
+            'index' => '品牌的管理',
+            'add' => '增加品牌',
+            'edit' => '编辑品牌',
+            'delete' => '移除品牌'
+        );
+        $meta_title = $meta_title[ACTION_NAME];
+        $this->assign("meta_title",$meta_title);
+        $this->_model = D('Brand');
+    }
+
+    /**
+     * 品牌的展示  index方法
+     */
+    public function index() {
+        //获得数据
+     $rows = $this->_model->select();
+        //传递数据
+        $this->assign('rows',$rows);
+        //渲染视图
+        $this->display();
+    }
+     /**
+      * 品牌增加 add方法
+      */
+     public function add(){}
+}
+
